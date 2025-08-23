30f7c852a9d48e51de0ab78d1788da18705f7cf3
Danish Mir <76698292+mirdanish6594@users.noreply.github.com>
2025-08-23 23:57:10 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,1 @@
-# wsgi.py
-# This is the production entry point for the Gunicorn server.
 
-from app import app, socketio
-
-if __name__ == "__main__":
-    # The host must be 0.0.0.0 to be accessible from outside the container
-    socketio.run(app, host="0.0.0.0", port=5000)
