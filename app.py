33659f400f0924b8fcee713627fc97cc18f943ef
Danish Mir <76698292+mirdanish6594@users.noreply.github.com>
2025-08-23 23:06:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,3 @@ def download_file(filename):
         filename,
         as_attachment=True
     )
-
-# --- Main Entry Point ---
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
